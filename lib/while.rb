f382cf031f138a9b292e2c14b7a261d6ce48ffce
jasmineanove <jasmineanove@gmail.com>
2018-07-24 22:27:48 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,5 +1,5 @@
 def using_while
-	levitation_force = 6
+	levitation_force == 6
 	puts "Hey dood you gots any grapes uh you gotta get level 10 helmet"
 	puts "YOU CURRENTLY HAVE #{levitation_force += 1} LEVL HELMET"
 end
